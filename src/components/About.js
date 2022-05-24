1380a89101ab90116955b05985c3e9300fb54de9
sekhar <sekhar.reddy000@gmail.com>
2022-05-24 11:34:02 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const About = () => {
+  return <div>This is About page</div>;
+};
+
+export default About;
